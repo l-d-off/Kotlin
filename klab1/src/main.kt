765d71be00be8c146d07d867309308050b660810
lsd-arf <vlad_lcd@mail.ru>
2021-04-08 21:01:28 +0300

    Задание 7 "Работа над числами". Вынести нахождение суммы цифр числа в отдельный метод. Реализовать методы поиска максимальной и минимальной цифры числа, произведения цифр чисда. Не удалять файл, в следующих лабораторных будет внесена модификация.

--- klab1/src/main.kt
+++ klab1/src/main.kt
@@ -67,16 +67,49 @@ fun main(args: Array<String>)
 }
 */
 
+// Задание 7. Работа с числами.
+// сумма цифр числа
 fun sumOfNumber(number: Int, sum: Int = 0) : Int
-        = if (number == 0)
+= if (number == 0)
     sum
 else
-{
-    val digit = number % 10
-    sumOfNumber(number / 10, sum + digit)
-}
+    sumOfNumber(number / 10, sum + (number % 10))
+
+// произведение цифр числа
+fun prodOfNumber(number: Int, prod: Int = 1) : Int
+= if (number == 0)
+    prod
+else
+    prodOfNumber(number / 10, prod * (number % 10))
+
+// минимальная цифра числа
+fun minDigitInNumber(number: Int, min: Int = 9) : Int
+= if (number == 0)
+    min
+else
+    minDigitInNumber(number / 10, (if (number % 10 < min) number % 10 else min))
+
+// максимальная цифра числа
+fun maxDigitInNumber(number: Int, max: Int = 0) : Int
+= if (number == 0)
+    max
+else
+    maxDigitInNumber(number / 10, (if (number % 10 > max) number % 10 else max))
 
 fun main(args: Array<String>)
 {
-    
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        println("Сумма цифр числа $number равна ${sumOfNumber(number)}")
+        println("Произведение цифр числа $number равно ${
+            if (number != 0) prodOfNumber(number) else 0}")
+        println("Минимальная цифра числа $number равна ${
+            if (number != 0) minDigitInNumber(number) else 0}")
+        println("Максимальная цифра числа $number равна ${maxDigitInNumber(number)}")
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
 }
