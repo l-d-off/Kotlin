70181e311d1550f0d26836df2b3f2c806cee40b3
lsd-arf <vlad_lcd@mail.ru>
2021-04-08 20:34:17 +0300

    Задание 6 Новая программа "Работа с числами". Найти сумму цифр числа. Рассмотреть класс целое число. Разобрать и включить в отчёт половину методов с использованием официальной документации языка.

--- klab1/src/main.kt
+++ klab1/src/main.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>)
 }
 */
 
+/* Задание 5
 fun main(args: Array<String>)
 {
     print("Your favourite language -> ")
@@ -33,4 +34,49 @@ fun main(args: Array<String>)
             else -> "Ты надлиза!"
         }
     println(output)
+}
+*/
+
+/* Задание 6. Работа с числами. Операторы ?. и ?:
+fun sumOfNumber(number: Int, sum: Int = 0) : Int
+= if (number == 0)
+        sum
+    else
+    {
+        val digit = number % 10
+        sumOfNumber(number / 10, sum + digit)
+    }
+
+fun main(args: Array<String>)
+{
+    /*
+    Использование ?. вызывает метод только в том случае, если значение не равно null,
+    в противном случае он просто передает null.
+    Использование ?: означает, что значение слева возвращается, если оно не является null,
+    в противном случае возвращается значение справа
+     */
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        println("Сумма цифр числа $number равна ${sumOfNumber(number)}")
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
+}
+*/
+
+fun sumOfNumber(number: Int, sum: Int = 0) : Int
+        = if (number == 0)
+    sum
+else
+{
+    val digit = number % 10
+    sumOfNumber(number / 10, sum + digit)
+}
+
+fun main(args: Array<String>)
+{
+    
 }
