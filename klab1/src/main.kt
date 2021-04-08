ad1b7aa0e603875ff6cc201d5c16b3a046353165
lsd-arf <vlad_lcd@mail.ru>
2021-04-08 17:29:44 +0300

    Задание 5 Продолжение задания 3 Спросить у пользователя, какой язык у него любимый, если это Kotlin или Prolog, ответь пользователю, что он - подлиза, для других языков придумать комментарий, воспользоваться для решения задачи условным оператором и операторм when.

--- klab1/src/main.kt
+++ klab1/src/main.kt
@@ -1,4 +1,36 @@
+/* задание 3
 fun main(args: Array<String>)
 {
     println("Hello, ${args[0]}!")
+}
+*/
+
+fun main(args: Array<String>)
+{
+    print("Your favourite language -> ")
+    val language = readLine()
+    /*
+    // first type (if)
+    if (language.equals("Kotlin") ||
+        language.equals("Prolog"))
+        println("Ты подлиза!")
+    else
+        println("Ты надлиза!")
+        */
+    /*
+    // second type (when)
+    when (language)
+    {
+        "Kotlin", "Prolog" -> println("Ты подлиза!")
+        else -> println("Ты надлиза!")
+    }
+    */
+    // third type (when)
+    val output =
+        when (language) // или when(readLine())
+        {
+            "Kotlin", "Prolog" -> "Ты подлиза!"
+            else -> "Ты надлиза!"
+        }
+    println(output)
 }
