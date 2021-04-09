eb9f5bb077cd33f3e7cf3c2e6e3b3f19b01b6951
lsd-arf <vlad_lcd@mail.ru>
2021-04-09 20:21:24 +0300

    Задание 8. Вариант 12. Метод 2. Найти количество цифр числа, меньших 3.

--- klab1/src/main.kt
+++ klab1/src/main.kt
@@ -126,7 +126,7 @@ fun simpleNumber(number: Int, del: Int = number - 1) : Boolean =
         else -> simpleNumber(number, del - 1)
     }
 
-// сумма непростых делителей числа
+// Подзадание 1. Сумма непростых делителей числа.
 fun sumOfNoSimpleDelOfNumber(number: Int, del: Int = number, sum: Int = 0) : Int =
     when
     {
@@ -136,12 +136,60 @@ fun sumOfNoSimpleDelOfNumber(number: Int, del: Int = number, sum: Int = 0) : Int
         else -> sumOfNoSimpleDelOfNumber(number, del - 1, sum)
     }
 
+// Подзадание 2. Количество цифр числа, меньших 3.
+fun countDigitsLess3(number: Int, count: Int = 0) : Int =
+    when
+    {
+        (number == 0) -> count
+        (number % 10 < 3) -> countDigitsLess3(number / 10, count + 1)
+        else -> countDigitsLess3(number / 10, count)
+    }
+
+// Подзадание 3. Количество чисел, которые
+// не являются делителями исходного числа
+// не взаимно простые с ним
+// взаимно простые с суммой простых цифр этого числа
+
+// НОД двух чисел
+fun nodTwoNumbers(number1: Int, number2: Int, nod: Int = (if (number1 > number2) number2 else number1)) : Int =
+    when
+    {
+        (number1 == 0 || number2 == 0) -> -1
+        (number1 % nod == 0 && number2 % nod == 0) -> nod
+        else -> nodTwoNumbers(number1, number2, nod - 1)
+    }
+
+// Сумма простых цифр числа
+fun sumOfSimpleDigitsOfNumber(number: Int, sum: Int = 0) : Int =
+    when
+    {
+        (number == 0) -> sum
+        simpleNumber(number % 10) -> sumOfSimpleDigitsOfNumber(number / 10, sum + (number % 10))
+        else -> sumOfSimpleDigitsOfNumber(number / 10, sum)
+    }
+/*
+// Основное подзадание 3
+fun task8method3(number: Int, del: Int = number - 1, count: Int = 0) : Int =
+    when
+    {
+        (del == -1 || del == 0) -> 0
+        (del == 1) -> count
+        (
+            number % del != 0 &&
+            nodTwoNumbers(number, del) != 1 &&
+            nodTwoNumbers(sumOfSimpleDigitsOfNumber(number), del) == 1
+        ) -> task8method3(number, del - 1, count + 1)
+        else -> task8method3(number, del - 1, count)
+    }
+*/
 fun main(args: Array<String>)
 {
     print("Number -> ")
     try {
         val number = readLine()?.toInt() ?: 0
-        println("Сумма непростых делителей числа $number равна ${sumOfNoSimpleDelOfNumber(number)}")
+        println("Результат задания 1: ${sumOfNoSimpleDelOfNumber(number)}")
+        println("Результат задания 2: ${countDigitsLess3(number)}")
+        /*println("Результат задания 3: ${task8method3(number)}")*/
     }
     catch (ex: NumberFormatException)
     {
