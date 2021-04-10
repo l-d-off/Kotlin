26bb767cf9b04dee21e314b58519a24b721b3947
lsd-arf <vlad_lcd@mail.ru>
2021-04-10 18:54:40 +0300

    Лабораторная работа 14. Задание 2. Написать рекурсивную функцию с телом выражения для нахождения суммы цифр числа. Использовать рекурсию вниз. Написать рекурсивную функцию с телом выражения для нахождения суммы цифр числа на основе хвостовой рекурсии.

--- klab2/src/main.kt
+++ klab2/src/main.kt
@@ -1,4 +1,4 @@
-// Задание 1. Работа с числами. Рекурсия вверх.
+/* Задание 1. Работа с числами. Рекурсия вверх.
 fun sumOfNumber(number: Int) : Int
 = if (number == 0)
     0
@@ -7,6 +7,38 @@ else
 
 fun main(args: Array<String>)
 {
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        println("Сумма цифр числа $number равна ${sumOfNumber(number)}")
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
+}
+*/
+
+// Задание 2. Работа с числами. Рекурсия вниз. Хвостовая рекурсия.
+fun sumOfNumber(number: Int, sum: Int = 0) : Int
+= if (number == 0)
+    sum
+else
+    sumOfNumber(number / 10, sum + number % 10)
+
+fun main(args: Array<String>)
+{
+    /*
+    Хвостовая рекурсия - это особый случай рекурсии, когда вызывающая функция
+    больше не выполняет вычисления после выполнения рекурсивного вызова.
+    Пример:
+    return 5
+    return sumOfNumber(a, b)
+    return sumOfNumber(a, b + number % 10)
+    Антипример:
+    return 5 * n
+    return n * sumOfNumber(a, b)
+     */
     print("Number -> ")
     try {
         val number = readLine()?.toInt() ?: 0
