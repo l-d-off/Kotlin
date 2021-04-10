1ba124aea56102beb0e98f0d4c60a24ac3cc38c4
lsd-arf <vlad_lcd@mail.ru>
2021-04-10 22:09:41 +0300

    Лабораторная работа 14. Задание 7. "Работа с числами". Переписать задание 8 из ЛР13 с использованием только хвостовой рекурсии и тела-выражения. Реализовать максимально возможное разделение на функции.

--- klab2/src/main.kt
+++ klab2/src/main.kt
@@ -52,7 +52,7 @@ fun main(args: Array<String>)
  */
 
 // Задание 3-4-5. Произведение, максимум, минимум. Рекурсия вверх. Хвостовая рекурсия.
-// Сумма цифр числа. Рекурсия вниз. Хвостовая рекурсия.
+/* Сумма цифр числа. Рекурсия вниз. Хвостовая рекурсия.
 fun sumOfNumber(number: Int, conditionFor: (Int) -> Boolean, sum: Int = 0) : Int
 = if (number == 0)
     sum
@@ -159,6 +159,7 @@ fun main(args: Array<String>)
         println("Некорректный ввод")
     }
 }
+ */
 
 /* Задание 6. Проверка.
 Функция-фильтр: цифра < 5
@@ -181,3 +182,111 @@ fun main(args: Array<String>)
 Результат: 32
 Пояснение: 4 * 4 * 2 = 32
 */
+
+// Задание 7. Работа с числами. Хвостовая рекурсия и тело-выражение.
+// проверка числа на простоту
+fun simpleNumber(number: Int, del: Int = number - 1) : Boolean =
+    when
+    {
+        number == 1 -> true
+        del == 1 -> true
+        number % del == 0 -> false
+        else -> simpleNumber(number, del - 1)
+    }
+
+// Подзадание 1. Сумма непростых делителей числа.
+fun sumOfNoSimpleDelOfNumber(number: Int, del: Int = number, sum: Int = 0) : Int =
+    when
+    {
+        (number == 0) -> 0
+        (del == 0) -> sum
+        (number % del == 0 && !simpleNumber(del)) -> sumOfNoSimpleDelOfNumber(number, del - 1, sum + del)
+        else -> sumOfNoSimpleDelOfNumber(number, del - 1, sum)
+    }
+
+// Подзадание 2. Количество цифр числа, меньших 3.
+fun countDigitsLess3(number: Int, count: Int = 0) : Int =
+    when
+    {
+        (number == 0) -> count
+        (number % 10 < 3) -> countDigitsLess3(number / 10, count + 1)
+        else -> countDigitsLess3(number / 10, count)
+    }
+
+// Подзадание 3. Количество чисел, которые
+// не являются делителями исходного числа
+// не взаимно простые с ним
+// взаимно простые с суммой простых цифр этого числа
+
+// НОД двух чисел
+fun nodTwoNumbers(number1: Int, number2: Int, nod: Int = (if (number1 > number2) number2 else number1)) : Int =
+    when
+    {
+        (number1 == 0 || number2 == 0) -> -1
+        (number1 % nod == 0 && number2 % nod == 0) -> nod
+        else -> nodTwoNumbers(number1, number2, nod - 1)
+    }
+
+// Сумма простых цифр числа
+fun sumOfSimpleDigitsOfNumber(number: Int, sum: Int = 0) : Int =
+    when
+    {
+        (number == 0) -> sum
+        simpleNumber(number % 10) -> sumOfSimpleDigitsOfNumber(number / 10, sum + (number % 10))
+        else -> sumOfSimpleDigitsOfNumber(number / 10, sum)
+    }
+
+// Основное подзадание 3
+fun task8method3(number: Int, del: Int = number - 1, count: Int = 0) : Int =
+    when
+    {
+        (del == -1 || del == 0) -> 0
+        (del == 1) -> count
+        (
+                number % del != 0 &&
+                        nodTwoNumbers(number, del) != 1 &&
+                        nodTwoNumbers(sumOfSimpleDigitsOfNumber(number), del) == 1
+                ) -> task8method3(number, del - 1, count + 1)
+        else -> task8method3(number, del - 1, count)
+    }
+
+fun continueMode() : String
+{
+    print("Continue? y/n -> ")
+    return when (readLine()) {
+        "y" -> "mainRelease"
+        "n" -> "return"
+        else -> continueMode()
+    }
+}
+
+fun mainRelease()
+{
+    print("Number -> ")
+    try {
+        val number = readLine()?.toInt() ?: 0
+        print("Method 1/2/3 -> ")
+        when(readLine()?.toInt() ?: 1)
+        {
+            1 -> println("Сумма непростых делителей числа $number: ${sumOfNoSimpleDelOfNumber(number)}")
+            2 -> println("Количество цифр числа $number, меньших 3: ${countDigitsLess3(number)}")
+            3 -> println("Количество чисел по заданию 3: ${task8method3(number)}")
+            else -> println("Такого метода нет в списке")
+        }
+    }
+    catch (ex: NumberFormatException)
+    {
+        println("Некорректный ввод")
+    }
+
+    when(continueMode())
+    {
+        "mainRelease" -> mainRelease()
+        "return" -> return
+    }
+}
+
+fun main(args: Array<String>)
+{
+    mainRelease()
+}
