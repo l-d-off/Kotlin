34aea7f813b9c8c86fb8a51c0669fcfd49d76968
lsd-arf <vlad_lcd@mail.ru>
2021-06-02 18:36:14 +0300

    Лабораторная работа 16. Задание 2. Задача 1.

--- klab4/src/main.kt
+++ klab4/src/main.kt
@@ -6,7 +6,10 @@ fun main() {
 //     print("Input string: ")
 //     val s = readLine()!!.toString()
 
-     task1()
+//     task1()
+
+//    task 2: задачи 3, 8, 16
+    // task2_launchMenu()
 }
 
 // вместо joinToString
@@ -35,4 +38,19 @@ fun task1() {
         println("Error: ${e.message}. Try again!\n")
         task1()
     }
+}
+
+// task 2.3 (1/3): дана строка в которой слова записаны через пробел,
+// перемешать все слова этой строке в случайном порядке
+fun task2_3(s: String): String {
+    fun replace(list: MutableList<String>, i: Int, j: Int) {
+        val temp = list[i]
+        list[i] = list[j]
+        list[j] = temp
+    }
+
+    val listOfWords = s.split(" ").toMutableList()
+    listOfWords.mapIndexed { i, _ ->  replace(listOfWords, i, Random.nextInt(0, listOfWords.size)) }
+
+    return listOfWords.joinToString(" ")
 }
