dd49c5e64132b7843c3a9b64b61cf8a38c9e97ed
lsd-arf <vlad_lcd@mail.ru>
2021-06-02 18:45:36 +0300

    Лабораторная работа 16. Задание 4. Задача 3. Дополнительно: добавлено меню к заданию 4.

--- klab4/src/main.kt
+++ klab4/src/main.kt
@@ -195,3 +195,48 @@ fun findLowercaseEnglishCharacters(s: String): Set<Char> {
     val lowercaseCharacters = s.toCharArray().filter { (it.toInt() >= 97) && (it.toInt() <= 122) }
     return lowercaseCharacters.toSet()
 }
+
+// task 4.16 (3/3): найти минимальное из имеющихся целых чисел
+fun findMinNumber(s: String): Int {
+    val regexNumbers = "\\d+".toRegex()
+    val listOfNumbers = regexNumbers.findAll(s).map { it.value.toInt() }.toList()
+
+    return listOfNumbers.minOrNull() ?: throw IllegalArgumentException("String doesn't contain numbers")
+}
+
+// task 4: предложить пользователю решить, какую задачу решать
+fun task4_launchMenu() {
+    println("What task do you want to complete?\n")
+    println("0. exit")
+    println("1. find number of russian characters")
+    println("2. find lowercase english characters")
+    println("3. find min number of string\n")
+    print(">: ")
+
+    val listTaskNumbers = listOf("1", "2", "3")
+    val choice = readLine()
+
+    val s = if (listTaskNumbers.contains(choice)) {
+        print("\nInput string: ")
+        readLine()!!.toString()
+    }
+    else ""
+
+    when(choice) {
+        "0" -> return
+        "1" -> println("Number of russian characters: ${findRussianCharacters(s)}")
+        "2" -> println("Lowercase english characters: ${findLowercaseEnglishCharacters(s)}")
+        "3" -> {
+            try {
+                println("Min number of string: ${findMinNumber(s)}")
+            }
+            catch(e: IllegalArgumentException) {
+                println("Error: ${e.message}. Try again!")
+            }
+        }
+        else -> println("Error: there's no such task. Try again!")
+    }
+
+    println()
+    task4_launchMenu()
+}
